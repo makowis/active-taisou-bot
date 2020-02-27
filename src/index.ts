551bcce80e35813dc308948641b57182c53a22df
Makoto Henmi <mako.mail.g@gmail.com>
2020-02-27 21:05:02 +0900

    アクティブ体操以外のメソッドを削除

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,5 @@
 import './lib/env' // 設定を.envからロード
 import * as Discord from 'discord.js'
-
-//ログイン処理
-const client = new Discord.Client();
-const token = process.env.DISCORD_TOKEN as string;
-
-client.on("ready", () => {
-  console.log("ready...");
-});
-
-client.on("message", message => {
-  if (message.author.bot) {
-    return;
-  }
-  
-  if (message.content.match(/ご安全に/)) {
-    let reply_text = `アクティブ体操やれ！ https://www.youtube.com/watch?v=KPxt7vyQ6Zo`;
-    reply(message, reply_text);
-    return;
-  }
-
-  if (message.content == 'oha') {
-    let reply_text = `おはようございますご主人様♪今日も1日頑張りましょう♪`;
-    reply(message, reply_text);
-    return;
-  }
-
-  if (message.content == 'ただいま') {
-    let reply_text = `おかえりなさいませ！ご主人様♪`;
-    reply(message, reply_text);
-    return;
-  }
-
-  if (message.content.match(/藤原竜也/)) {
-    let reply_text = `ど゛う゛し゛て゛な゛ん゛だ゛よ゛お゛お゛ぉ゛お゛！゛！゛！゛ん゛あ゛あ゛あ゛あ゛あ゛ぁ゛ぁ゛あ゛あ゛！゛！゛！゛！゛`;
-    reply(message, reply_text);
-    return;
-  }  
-});
-
-client.login(token);
-
 import { schedule } from 'node-cron';
 
 // Create a new webhook
@@ -66,11 +25,4 @@ const isWeekend = (today: Date) => {
   }
 
   return false;
-}
-
-const reply = (message: Discord.Message, reply_text: string) => {
-  message
-  .reply(reply_text)
-  .then(() => console.log(`Sent message: ${reply_text}`))
-  .catch(console.error);
 }
